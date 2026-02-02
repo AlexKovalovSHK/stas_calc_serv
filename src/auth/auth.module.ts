f3ab9b208dc204c605a6a3eddbdef9e9c04735e6
AlexKovalovSHK <alex@shk-info.de>
2026-02-02 15:15:55 +0100

    big update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport"; // 1. Импортируем PassportModule
 import * as dotenv from "dotenv";
 import { UserModule } from "src/user/user.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt-strategy";
 
 dotenv.config();
 
 @Module({
     imports: [
       UserModule,
+      // Регистрируем PassportModule и указываем стратегию по умолчанию
+      PassportModule.register({ defaultStrategy: 'jwt' }), 
       JwtModule.register({
         global: true,
         secret: process.env.PRIVATE_KEY || "secret_key",
@@ -18,9 +22,10 @@ dotenv.config();
         },
       }),
     ],
-    providers: [AuthService],
+    // 3. ОБЯЗАТЕЛЬНО добавляем JwtStrategy в providers
+    providers: [AuthService, JwtStrategy], 
     controllers: [AuthController],
-    exports: [JwtModule],
+    // Экспортируем всё необходимое для других модулей
+    exports: [AuthService, JwtModule, JwtStrategy, PassportModule], 
   })
-
   export class AuthModule {}
